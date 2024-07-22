36a413e384bf66a1b4dc27bc0a2fa2829879266f
Mertcan <98522906+fr0sty-m@users.noreply.github.com>
2024-07-22 23:57:45 +0300

    Add files via upload

---
+++ ch_spawn/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version  'cerulean'
+game        'gta5'
+lua54       'yes'
+
+
+author      'fr0sty'
+desciption ''
+resource_type 'gametype' { name = 'Tutorial Type!', }
+
+client_scripts {
+    'client/client.lua',
+}
+
+
